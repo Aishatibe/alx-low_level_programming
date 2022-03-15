cae75a7e5cbfdd4b57cc0614a8ac83cd294ba693
Aishatibe <suleaishat990@gmail.com>
2022-03-15 15:10:26 -0700

    prints alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - main block
+ * Description: prints alphabet in lower case
+ * Return: 0
+ */
+
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		putchar(la);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
