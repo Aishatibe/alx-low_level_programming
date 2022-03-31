3548ffec39f680e465d94d725633017eabbadb48
Aishatibe <suleaishat990@gmail.com>
2022-03-31 06:46:15 -0700

    recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,6 +11,10 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int is_palindrome(char *s);
+int _sqrt_rec(int n, int x);
+int is_prime(int n, int x);
+int palindrome(char *s, int first, int last);
+int _strlen_recursion(char *s);
 
 #endif
 
